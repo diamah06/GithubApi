f11171fd245acb65e4e4baedd2d941d211572b7f
diamah06 <tabalmahdia@yahoo.fr>
2022-09-27 14:05:31 +0200

    Initial Commit

---
+++ GithubApi/GithubApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApiApp.swift
+//  GithubApi
+//
+//  Created by Mahdia Amriou on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GithubApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
